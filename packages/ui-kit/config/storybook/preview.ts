9f189fe7167e5913b8340ba75470d61386ea2124
Yuri A Taranov <me@flanker72.ru>
2021-07-20 23:22:10 +0500

    Test git hooks

--- packages/ui-kit/config/storybook/preview.ts
+++ packages/ui-kit/config/storybook/preview.ts
@@ -1,9 +1,11 @@
-import { Decorators } from '@storybook/vue3';
+import { Decorators } from "@storybook/vue3";
 
 import "!style-loader!css-loader!less-loader!../../src/styles/common.less";
 import "!style-loader!css-loader!less-loader!../../src/styles/components.less";
 import "!style-loader!css-loader!less-loader!../../src/themes/light.less";
 
-export const decorators: Decorators = [() => ({
-  template: "<div class='vc-theme_light'><story/></div>"
-})];
+export const decorators: Decorators = [
+  () => ({
+    template: "<div class='vc-theme_light'><story/></div>",
+  }),
+];

57d84afae7eeaab6c501346150f18ce49f7c27b7
Yuri A Taranov <me@flanker72.ru>
2021-08-02 19:09:56 +0500

    feat: core composables

--- framework/core/src/index.ts
+++ framework/core/src/index.ts
@@ -1,7 +1,8 @@
 import { App } from "vue";
 import { init as initLogger } from "./composables/logger";
+import { init as initI18n } from "./composables/i18n";
 
-const init = [initLogger];
+const init = [initLogger, initI18n];
 
 export default {
   install(app: App): void {
--- framework/core/vue.config.js
+++ framework/core/vue.config.js
@@ -8,26 +8,26 @@ module.exports = {
     config.plugins.delete('html');
     config.plugins.delete('preload');
     config.plugins.delete('prefetch');
-    config.externals([
-      "vue-i18n",
-      "@virtoshell/ui",
-      "vue-logger-plugin",
-      "vue-router"
-    ]);
     config.module.rule('ts').uses.delete('thread-loader');
     config.module
       .rule('ts')
       .use('ts-loader')
       .tap(options => {
-        options.transpileOnly = false
-        options.happyPackMode = false
+        options.transpileOnly = false;
+        options.happyPackMode = false;
         options.compilerOptions = {
           declaration: true,
           noEmit: false,
-          outDir: 'dist'
+          outDir: 'dist',
         };
         return options;
       })
+    config.externals([
+      "vue-i18n",
+      "@virtoshell/ui",
+      "vue-logger-plugin",
+      "vue-router",
+    ]);
   },
 
   productionSourceMap: false,

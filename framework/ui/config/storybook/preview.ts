c27dba0e054e85a8a7f5539d2e09f864cd3dfd50
Yuri A Taranov <me@flanker72.ru>
2021-08-05 12:47:53 +0500

    fix: storybook build pipeline

--- framework/ui/config/storybook/preview.ts
+++ framework/ui/config/storybook/preview.ts
@@ -5,7 +5,7 @@ import "!style-loader!css-loader!less-loader!../../src/styles/index.less";
 import "!style-loader!css-loader!@virtoshell/ui-theme-light/dist/theme.css";
 
 export const decorators: Decorators = [
-  (): unknown => ({
+  () => ({
     template: "<div class='vc-theme_light'><story/></div>",
   }),
 ];

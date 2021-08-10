5a9b0b76b97eb16def289f1a89da18a7f3034e9e
Yuri A Taranov <me@flanker72.ru>
2021-08-10 21:10:28 +0500

    feat(ui): vc-button component

--- framework/ui/src/components/atoms/vc-button/vc-button.vue
+++ framework/ui/src/components/atoms/vc-button/vc-button.vue
@@ -5,9 +5,15 @@
       `vc-button_${variant}`,
       { 'vc-button_disabled': disabled, 'vc-button_small': small },
     ]"
+    @click="onClick"
   >
-    <i v-if="icon" class="vc-button__icon"></i>
-    <div class="vc-button__title">
+    <vc-icon
+      v-if="icon"
+      class="vc-button__icon"
+      :icon="icon"
+      :size="small ? 'xs' : 's'"
+    ></vc-icon>
+    <div v-if="$slots['default']" class="vc-button__title">
       <slot></slot>
     </div>
   </div>
@@ -15,11 +21,17 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import VcIcon from "../vc-icon/vc-icon.vue";
 
 export default defineComponent({
+  components: {
+    VcIcon,
+  },
+
   props: {
     icon: {
       type: String,
+      default: undefined,
     },
 
     variant: {
@@ -30,6 +42,7 @@ export default defineComponent({
 
     disabled: {
       type: Boolean,
+      default: false,
     },
 
     small: {
@@ -37,33 +50,160 @@ export default defineComponent({
       default: false,
     },
   },
+
+  emits: ["click"],
+
+  setup(props, { emit }) {
+    return {
+      onClick(): void {
+        if (!props.disabled) {
+          emit("click");
+        }
+      },
+    };
+  },
 });
 </script>
 
 <style lang="less">
+:root {
+  --button-primary-background-color: #41afe6;
+  --button-primary-background-color-hover: #319ed4;
+  --button-primary-background-color-active: #319ed4;
+  --button-primary-background-color-disabled: #a9ddf6;
+
+  --button-primary-text-color: #ffffff;
+  --button-primary-text-color-hover: #ffffff;
+  --button-primary-text-color-active: #ffffff;
+  --button-primary-text-color-disabled: #ffffff;
+
+  --button-secondary-background-color: #ffffff;
+  --button-secondary-background-color-hover: #ffffff;
+  --button-secondary-background-color-active: #ffffff;
+  --button-secondary-background-color-disabled: #ffffff;
+
+  --button-secondary-border-color: #43b0e6;
+  --button-secondary-border-color-hover: #319ed4;
+  --button-secondary-border-color-active: #319ed4;
+  --button-secondary-border-color-disabled: #a9ddf6;
+
+  --button-secondary-text-color: #43b0e6;
+  --button-secondary-text-color-hover: #319ed4;
+  --button-secondary-text-color-active: #319ed4;
+  --button-secondary-text-color-disabled: #a9ddf6;
+
+  --button-special-background-color: #f89406;
+  --button-special-background-color-hover: #eb8b03;
+  --button-special-background-color-active: #eb8b03;
+  --button-special-background-color-disabled: #fed498;
+
+  --button-special-text-color: #ffffff;
+  --button-special-text-color-hover: #ffffff;
+  --button-special-text-color-active: #ffffff;
+  --button-special-text-color-disabled: #ffffff;
+
+  --button-border-radius: 2px;
+  --button-padding: 0 14px;
+  --button-padding-small: 0 12px;
+  --button-height: 28px;
+  --button-height-small: 22px;
+}
+
 .vc-button {
-  border-radius: 2px;
-  padding: 0 28px;
-  height: var(--vc-button-height);
+  border-radius: var(--button-border-radius);
+  padding: var(--button-padding);
+  height: var(--button-height);
   display: inline-flex;
   align-items: center;
   font-weight: var(--font-weight-medium);
   font-size: var(--font-size-s);
   cursor: pointer;
+  box-sizing: border-box;
+  transition: all 0.2s ease;
 
-  &_special {
-    background: var(--vc-button-special-color);
-    color: #ffffff;
+  &__icon + &__title {
+    margin-left: var(--margin-s);
+  }
+
+  &_primary {
+    background-color: var(--button-primary-background-color);
+    color: var(--button-primary-text-color);
 
     &:hover {
-      background: var(--vc-button-special-color-hover);
+      background-color: var(--button-primary-background-color-hover);
+      color: var(--button-primary-text-color-hover);
+    }
+
+    &:focus {
+      background-color: var(--button-primary-background-color-active);
+      color: var(--button-primary-text-color-active);
+    }
+
+    &.vc-button_disabled,
+    &.vc-button_disabled:hover {
+      cursor: not-allowed;
+      background-color: var(--button-primary-background-color-disabled);
+      color: var(--button-primary-text-color-disabled);
+    }
+  }
+
+  &_secondary {
+    background-color: var(--button-secondary-background-color);
+    color: var(--button-secondary-text-color);
+    border: 1px solid var(--button-secondary-border-color);
+
+    &:hover {
+      background-color: var(--button-secondary-background-color-hover);
+      color: var(--button-secondary-text-color-hover);
+      border: 1px solid var(--button-secondary-border-color-hover);
+    }
+
+    &:focus {
+      background-color: var(--button-secondary-background-color-active);
+      color: var(--button-secondary-text-color-hover);
+      border: 1px solid var(--button-secondary-border-color-active);
+    }
+
+    &.vc-button_disabled,
+    &.vc-button_disabled:hover {
+      cursor: not-allowed;
+      background-color: var(--button-secondary-background-color-disabled);
+      color: var(--button-secondary-text-color-disabled);
+      border: 1px solid var(--button-secondary-border-color-disabled);
+    }
+  }
+
+  &_special {
+    background-color: var(--button-special-background-color);
+    color: var(--button-special-text-color);
+
+    &:hover {
+      background-color: var(--button-special-background-color-hover);
+      color: var(--button-special-text-color-hover);
+    }
+
+    &:focus {
+      background-color: var(--button-special-background-color-active);
+      color: var(--button-special-text-color-active);
+    }
+
+    &.vc-button_disabled,
+    &.vc-button_disabled:hover {
+      cursor: not-allowed;
+      background-color: var(--button-special-background-color-disabled);
+      color: var(--button-special-text-color-disabled);
     }
   }
 
   &_small {
-    height: var(--vc-button-small-height);
+    height: var(--button-height-small);
+    padding: var(--button-padding-small);
     font-weight: var(--font-weight-normal);
     font-size: var(--font-size-xs);
+
+    .vc-button__icon + .vc-button__title {
+      margin-left: var(--margin-xs);
+    }
   }
 }
 </style>

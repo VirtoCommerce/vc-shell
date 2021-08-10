adc464d741f2621288578c4e3a7d3e726d367bd1
Yuri A Taranov <me@flanker72.ru>
2021-08-10 21:10:05 +0500

    feat(ui): storybook icons and fonts

--- framework/ui/config/storybook/preview.ts
+++ framework/ui/config/storybook/preview.ts
@@ -1,11 +1,13 @@
 /* eslint-disable */
 import { Decorators } from "@storybook/vue3";
 
+import "@fortawesome/fontawesome-free/css/all.css";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "!style-loader!css-loader!less-loader!../../src/styles/index.less";
 import "!style-loader!css-loader!@virtoshell/ui-theme-light/dist/theme.css";
 
 export const decorators: Decorators = [
   () => ({
-    template: "<div class='vc-theme_light'><story/></div>",
+    template: "<div class='vc-theme_light' style='font-family: \"Roboto\"; font-size: var(--font-size-m)'><story/></div>",
   }),
 ];

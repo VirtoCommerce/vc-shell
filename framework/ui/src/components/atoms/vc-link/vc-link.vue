cdf34faebca3fa4cba91c4f14e8fe239995da5b1
Yuri A Taranov <me@flanker72.ru>
2021-08-10 19:23:15 +0500

    feat(ui): vc-link component

--- framework/ui/src/components/atoms/vc-link/vc-link.vue
+++ framework/ui/src/components/atoms/vc-link/vc-link.vue
@@ -1,7 +1,12 @@
 <template>
-  <a class="vc-link" :class="{ 'vc-link_disabled': disabled }" :href="to">
+  <router-link
+    class="vc-link"
+    :class="{ 'vc-link_active': active, 'vc-link_disabled': disabled }"
+    :href="disabled ? null : to"
+    @click="onClick"
+  >
     <slot></slot>
-  </a>
+  </router-link>
 </template>
 
 <script lang="ts">
@@ -11,6 +16,12 @@ export default defineComponent({
   props: {
     to: {
       type: String,
+      default: undefined,
+    },
+
+    active: {
+      type: Boolean,
+      default: false,
     },
 
     disabled: {
@@ -18,23 +29,51 @@ export default defineComponent({
       default: false,
     },
   },
+
+  emits: ["click"],
+
+  setup(props, { emit }) {
+    return {
+      onClick(): void {
+        if (!props.disabled) {
+          emit("click");
+        }
+      },
+    };
+  },
 });
 </script>
 
 <style lang="less">
+:root {
+  --link-text-color: hsl(200, 77%, 58%);
+  --link-text-color-hover: hsl(200, 77%, 48%);
+  --link-text-color-active: hsl(200, 77%, 48%);
+  --link-text-color-disabled: hsl(200, 77%, 73%);
+}
+
 .vc-link {
-  color: var(--basic-black-color);
+  color: var(--link-text-color);
   text-decoration: none;
   cursor: pointer;
+  transition: all 0.2s ease;
 
   &:hover {
+    color: var(--link-text-color-hover);
     text-decoration: underline;
+  }
+
+  &_active {
+    color: var(--link-text-color-active);
+    text-decoration: none;
   }
 
   &_disabled {
     cursor: not-allowed;
+    color: var(--link-text-color-disabled);
 
     &:hover {
+      color: var(--link-text-color-disabled);
       text-decoration: none;
     }
   }

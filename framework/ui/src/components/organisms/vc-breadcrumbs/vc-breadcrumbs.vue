dea9e8940c18daaefb743fc8df0e0dc50f25d28a
Yuri A Taranov <me@flanker72.ru>
2021-08-11 18:45:20 +0500

    feat(ui): vc-breadcrumbs component update

--- framework/ui/src/components/organisms/vc-breadcrumbs/vc-breadcrumbs.vue
+++ framework/ui/src/components/organisms/vc-breadcrumbs/vc-breadcrumbs.vue
@@ -1,40 +1,27 @@
 <template>
-  <div
-    v-if="items"
-    class="vc-breadcrumbs vc-flex vc-flex-align_center vc-flex-nowrap"
-  >
-    <template v-for="(item, i) in items" :key="item.id">
-      <div
-        class="vc-breadcrumbs__item vc-flex vc-flex-align_center"
-        :class="{
-          'vc-breadcrumbs__item_disabled': item.disabled,
-          'vc-breadcrumbs__item_current': i === items.length - 1,
-        }"
-      >
-        <vc-icon
-          v-if="item.icon"
-          class="vc-breadcrumbs__item-icon"
-          :icon="item.icon"
-          size="s"
-        />
-        <div class="vc-breadcrumbs__item-title">
-          {{ item.title }}
-        </div>
-      </div>
-    </template>
+  <div v-if="items" class="vc-breadcrumbs">
+    <vc-breadcrumbs-item
+      v-for="(item, i) in items"
+      :key="item.id"
+      v-bind="item"
+      :current="i === items.length - 1"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import VcIcon from "../../atoms/vc-icon/vc-icon.vue";
+import VcBreadcrumbsItem from "../../molecules/vc-breadcrumbs-item/vc-breadcrumbs-item.vue";
 
 export default defineComponent({
-  components: { VcIcon },
+  components: {
+    VcBreadcrumbsItem,
+  },
 
   props: {
     items: {
       type: Array,
+      default: () => [],
     },
   },
 });
@@ -42,40 +29,8 @@ export default defineComponent({
 
 <style lang="less">
 .vc-breadcrumbs {
-  &__item {
-    height: 28px;
-    box-sizing: border-box;
-    border-radius: 14px;
-    border: 1px solid #a1c0d4;
-    color: #43b0e6;
-    white-space: nowrap;
-    padding: 0 14px;
-    margin-right: 6px;
-    font-size: var(--font-size-s);
-    cursor: pointer;
-
-    &:hover {
-      border: 1px solid #8fb0c6;
-    }
-
-    &-icon {
-      margin-right: 6px;
-      color: #a1c0d4;
-    }
-
-    &_disabled {
-      opacity: 0.4;
-    }
-
-    &_current {
-      color: #465769;
-      border: 1px solid #838d9a;
-      cursor: default;
-
-      &:hover {
-        border: 1px solid #838d9a;
-      }
-    }
-  }
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
 }
 </style>

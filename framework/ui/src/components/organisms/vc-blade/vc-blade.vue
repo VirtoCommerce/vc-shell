2c3fe2bf56a70dff938b496d6ec50353bda06ce4
Yuri A Taranov <me@flanker72.ru>
2021-08-11 17:37:15 +0500

    feat(ui): vc-blade component update

--- framework/ui/src/components/organisms/vc-blade/vc-blade.vue
+++ framework/ui/src/components/organisms/vc-blade/vc-blade.vue
@@ -4,54 +4,18 @@
     :style="{ width: `${width}px` }"
     :class="{ 'vc-blade_expanded': expanded }"
   >
-    <div class="vc-blade__topbar vc-flex-shrink_0">
-      <div
-        v-if="expanded"
-        class="vc-blade__topbar-button"
-        @click="expanded = false"
-      >
-        <vc-icon icon="window-minimize" size="s"></vc-icon>
-      </div>
-      <div v-else class="vc-blade__topbar-button" @click="expanded = true">
-        <vc-icon icon="window-maximize" size="s"></vc-icon>
-      </div>
-      <div
-        class="vc-blade__topbar-button"
-        :class="{ 'vc-blade__topbar-button_disabled': !closable }"
-        @click="closable && $emit('close')"
-      >
-        <vc-icon icon="times"></vc-icon>
-      </div>
-    </div>
+    <vc-blade-header
+      :expanded="expanded"
+      :closable="closable"
+      :icon="icon"
+      :title="title"
+      :subtitle="subtitle"
+      @expand="expanded = true"
+      @collapse="expanded = false"
+      @close="$emit('close')"
+    />
 
-    <div class="vc-blade__header vc-flex-shrink_0">
-      <div class="vc-blade__header-icon">
-        <vc-icon :icon="icon" size="xxl"></vc-icon>
-      </div>
-      <div class="vc-blade__header-info">
-        <div
-          class="vc-blade__header-title"
-          :class="{ 'vc-blade__header-title_only': !subtitle }"
-        >
-          {{ title }}
-        </div>
-        <div v-if="subtitle" class="vc-blade__header-subtitle">
-          {{ subtitle }}
-        </div>
-      </div>
-    </div>
-
-    <div v-if="toolbarItems" class="vc-blade__toolbar vc-flex-shrink_0">
-      <template v-for="item in toolbarItems" :key="item.id">
-        <div
-          class="vc-blade__toolbar-item"
-          :class="{ 'vc-blade__toolbar-item_disabled': item.disabled }"
-        >
-          <vc-icon :icon="item.icon" size="ss"></vc-icon>
-          <div class="vc-blade__toolbar-item-title">{{ item.title }}</div>
-        </div>
-      </template>
-    </div>
+    <vc-blade-toolbar :items="toolbarItems" />
 
     <vc-breadcrumbs
       v-if="breadcrumbs"
@@ -132,11 +96,19 @@
 import { defineComponent, ref } from "vue";
 import VcIcon from "../../atoms/vc-icon/vc-icon.vue";
 import VcFormInput from "../../molecules/vc-form-input/vc-form-input.vue";
+import VcBladeHeader from "../../molecules/vc-blade-header/vc-blade-header.vue";
+import VcBladeToolbar from "../../organisms/vc-blade-toolbar/vc-blade-toolbar.vue";
 import VcBreadcrumbs from "../../organisms/vc-breadcrumbs/vc-breadcrumbs.vue";
 
 export default defineComponent({
   name: "VcBlade",
-  components: { VcIcon, VcFormInput, VcBreadcrumbs },
+  components: {
+    VcIcon,
+    VcFormInput,
+    VcBladeHeader,
+    VcBladeToolbar,
+    VcBreadcrumbs,
+  },
 
   props: {
     icon: {
@@ -194,98 +166,11 @@ export default defineComponent({
 .vc-blade {
   display: flex;
   flex-direction: column;
-  border-right: 1px solid var(--vc-blade-border-color);
-  background: #ffffff;
+  border-right: 1px solid var(--blade-border-color);
+  background: var(--blade-background-color);
 
   &_expanded {
     width: 100% !important;
-  }
-
-  &__topbar {
-    height: var(--vc-blade-topbar-height);
-    background-color: var(--vc-blade-topbar-background-color);
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-
-    &-button {
-      color: var(--vc-blade-topbar-button-color);
-      margin-right: 16px;
-      cursor: pointer;
-
-      &_disabled {
-        color: var(--vc-blade-topbar-button-disabled-color);
-        cursor: not-allowed;
-      }
-    }
-  }
-
-  &__header {
-    height: var(--vc-blade-header-height);
-    background-color: var(--vc-blade-header-background-color);
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    padding: 0 16px;
-
-    &-icon {
-      color: var(--vc-blade-header-icon-color);
-      margin-right: 13px;
-    }
-
-    &-title {
-      color: var(--vc-blade-header-title-color);
-      font-size: var(--font-size-m);
-
-      &_only {
-        font-size: var(--font-size-xl);
-      }
-    }
-
-    &-subtitle {
-      color: var(--vc-blade-header-subtitle-color);
-      font-size: var(--font-size-xs);
-    }
-  }
-
-  &__toolbar {
-    height: var(--vc-blade-toolbar-height);
-    background-color: var(--vc-blade-toolbar-background-color);
-    display: flex;
-    justify-content: flex-start;
-    align-items: stretch;
-    border-top: 1px solid rgba(255, 255, 255, 0.2);
-    padding: 0 8px;
-
-    &-item {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 0 8px;
-      color: var(--vc-blade-toolbar-item-color);
-      box-sizing: border-box;
-      cursor: pointer;
-
-      &-title {
-        font-size: var(--font-size-s);
-        white-space: nowrap;
-        margin-top: 4px;
-      }
-
-      &:hover {
-        background-color: var(--vc-blade-toolbar-item-hover-background-color);
-      }
-
-      &_disabled {
-        color: var(--vc-blade-toolbar-item-disabled-color);
-        cursor: default;
-
-        &:hover {
-          background-color: var(--vc-blade-toolbar-background-color);
-        }
-      }
-    }
   }
 
   &__searchbar {

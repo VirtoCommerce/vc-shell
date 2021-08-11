82face9ce96889aad8dfc83cba4db059997c76c0
Yuri A Taranov <me@flanker72.ru>
2021-08-11 22:06:57 +0500

    feat(ui): vc-tooltip component

--- framework/ui/src/components/atoms/vc-tooltip/vc-tooltip.vue
+++ framework/ui/src/components/atoms/vc-tooltip/vc-tooltip.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>vc-tooltip</div>
+  <div class="vc-tooltip">
+    <slot></slot>
+  </div>
 </template>
+
+<style lang="less">
+:root {
+  --tooltip-color: #a5a5a5;
+}
+
+.vc-tooltip {
+  color: var(--tooltip-color);
+  font-size: var(--font-size-xs);
+}
+</style>

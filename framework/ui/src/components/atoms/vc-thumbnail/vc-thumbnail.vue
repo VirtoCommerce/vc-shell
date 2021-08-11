f83e40195ec401a0eaf31ae527f9cd0b8dd9b3a6
Yuri A Taranov <me@flanker72.ru>
2021-08-11 13:06:58 +0500

    feat(ui): vc-thumbnail component

--- framework/ui/src/components/atoms/vc-thumbnail/vc-thumbnail.vue
+++ framework/ui/src/components/atoms/vc-thumbnail/vc-thumbnail.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>vc-thumbnail</div>
+  <div
+    class="vc-thumbnail"
+    :class="`vc-thumbnail_${aspect}`"
+    :style="{ background: `url(${src}) center / cover no-repeat` }"
+  ></div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    aspect: {
+      type: String,
+      default: "1x1",
+    },
+
+    src: {
+      type: String,
+      default: undefined,
+    },
+  },
+});
+</script>
+
+<style lang="less">
+:root {
+}
+</style>

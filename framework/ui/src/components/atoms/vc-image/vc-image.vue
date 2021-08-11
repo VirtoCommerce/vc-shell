b8c123e460bb58d570df780f31bc53d6a95f9468
Yuri A Taranov <me@flanker72.ru>
2021-08-11 21:39:37 +0500

    feat(ui): vc-image component

--- framework/ui/src/components/atoms/vc-image/vc-image.vue
+++ framework/ui/src/components/atoms/vc-image/vc-image.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>vc-image</div>
+  <img class="vc-image" :src="src" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    src: {
+      type: String,
+      default: undefined,
+    },
+  },
+});
+</script>
+
+<style lang="less">
+.vc-image {
+  max-width: 100%;
+}
+</style>

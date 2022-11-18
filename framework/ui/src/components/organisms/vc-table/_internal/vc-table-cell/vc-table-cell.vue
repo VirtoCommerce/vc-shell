93d75e9222797ef86f7fe25af99bedcf81faebd1
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2022-11-18 15:25:50 +0200

    VM-1066: Fix table support of date & time (#143)
    * fix: add support of nullable dates in VcTable
    
    * feature: add time, date-time cell types

--- framework/ui/src/components/organisms/vc-table/_internal/vc-table-cell/vc-table-cell.vue
+++ framework/ui/src/components/organisms/vc-table/_internal/vc-table-cell/vc-table-cell.vue
@@ -16,16 +16,36 @@
 
   <!-- Date ago cell -->
   <span v-else-if="cell.type === 'date-ago'" class="text-[#a5a5a5]">
-    {{ moment(value).fromNow() }}
+    <template v-if="value">
+      {{ moment(value).fromNow() }}
+    </template>
+    <template v-else>N/A</template>
   </span>
 
   <!-- Date exact cell -->
-  <span v-else-if="cell.type === 'date'" class="text-[#a5a5a5]">
-    {{
-      cell.format
-        ? moment(value).locale(locale).format(cell.format)
-        : value.toLocaleDateString()
-    }}
+  <span
+    v-else-if="
+      cell.type === 'date' || cell.type === 'time' || cell.type === 'date-time'
+    "
+    class="text-[#a5a5a5]"
+  >
+    <template v-if="value">
+      <template v-if="cell.format">
+        {{ moment(value).locale(locale).format(cell.format) }}
+      </template>
+      <template v-else>
+        <template v-if="cell.type === 'date'">{{
+          value.toLocaleDateString()
+        }}</template>
+        <template v-if="cell.type === 'time'">{{
+          value.toLocaleTimeString()
+        }}</template>
+        <template v-if="cell.type === 'date-time'">{{
+          value.toLocaleString()
+        }}</template>
+      </template>
+    </template>
+    <template v-else>N/A</template>
   </span>
 
   <!-- Image cell -->

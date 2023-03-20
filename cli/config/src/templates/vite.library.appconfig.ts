42d9d9700ea47027b3bd7d6f88d6254ab4b63020
symbot <maxim.zinchuk@gmail.com>
2023-03-20 16:32:20 +0300

    fix: deps fix

--- cli/config/src/templates/vite.library.appconfig.ts
+++ cli/config/src/templates/vite.library.appconfig.ts
@@ -1,6 +1,4 @@
-import typescript from "@rollup/plugin-typescript";
 import { LibraryOptions, UserConfigExport } from "vite";
-import { RollupOptions } from "rollup";
 
 export default {
   build: {

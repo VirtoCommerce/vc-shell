d0db9aec6cfddc4d5b9180f51be21a32bffd721e
Maksim Zinchuk <63730586+maksimzinchuk@users.noreply.github.com>
2023-03-22 15:42:11 +0200

    Update products-edit.vue
    fix: fixed add new product

--- src/modules/products/pages/products-edit.vue
+++ src/modules/products/pages/products-edit.vue
@@ -15,7 +15,7 @@
     <!-- Blade contents -->
     <VcContainer :no-padding="true">
       <div
-        v-if="productData.id"
+        v-if="!(loading || productLoading)"
         class="product-details__inner"
       >
         <div class="product-details__content">

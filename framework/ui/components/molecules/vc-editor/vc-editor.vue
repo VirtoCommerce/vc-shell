b494b9c3439e2d52d4be80616712b86b8126f799
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2023-08-01 17:26:27 +0200

    feat: vc-editor multilang

--- framework/ui/components/molecules/vc-editor/vc-editor.vue
+++ framework/ui/components/molecules/vc-editor/vc-editor.vue
@@ -63,10 +63,13 @@ export interface Props {
   tooltip?: string;
   errorMessage?: string;
   assetsFolder: string;
+  languages?: string[];
+  currentLanguage?: string;
 }
 
 export interface Emits {
   (event: "update:modelValue", value: string | number | Date | null | undefined): void;
+  (event: "update:currentLanguage", value: string): void;
 }
 
 const { getAccessToken } = useUser();
@@ -79,19 +82,27 @@ const props = withDefaults(defineProps<Props>(), {
 const emit = defineEmits<Emits>();
 
 const content = ref();
-const toolbar = [
-  { header: 1 },
-  { header: 2 },
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "link",
-  "image",
-  "blockquote",
-  { list: "ordered" },
-  { list: "bullet" },
-];
+const toolbar = {
+  container: [
+    { header: 1 },
+    { header: 2 },
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "link",
+    "image",
+    "blockquote",
+    { list: "ordered" },
+    { list: "bullet" },
+    { language: [props.currentLanguage, ...props.languages.filter((lang) => lang !== props.currentLanguage)] },
+  ],
+  handlers: {
+    language: function (value: string) {
+      emit("update:currentLanguage", value);
+    },
+  },
+};
 
 const modules = {
   name: "imageUploader",
@@ -175,5 +186,13 @@ function isQuillEmpty(value: string) {
       hover:tw-bg-[color:var(--editor-scroll-color-hover)];
     }
   }
+}
+
+.ql-language.ql-picker .ql-picker-label:before {
+  padding-right: 18px;
+  content: attr(data-value);
+}
+.ql-language.ql-picker .ql-picker-item:before {
+  content: attr(data-value);
 }
 </style>

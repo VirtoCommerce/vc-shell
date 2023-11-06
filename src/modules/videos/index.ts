c18e05701e24880935c3275a0dcfc5913b614e1a
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2023-11-06 16:33:35 +0100

    chore: video module cleaning

--- src/modules/videos/index.ts
+++ src/modules/videos/index.ts
@@ -1,25 +1,14 @@
-// import * as pages from "./pages";
-// import * as locales from "./locales";
-// import { createAppModule } from "@vc-shell/framework";
-
-// export default createAppModule(pages, locales);
-
-// export * from "./pages";
-// export * from "./composables";
-
 import * as components from "./components";
 import * as schema from "./pages";
 import * as locales from "./locales";
 import * as composables from "./composables";
-//import * as notificationTemplates from "./components/notifications";
 import { createDynamicAppModule } from "@vc-shell/framework";
 
 export default createDynamicAppModule({
   schema,
   composables,
   moduleComponents: components,
-  //  notificationTemplates,
   locales,
 });
 
-export { components, schema, locales, composables /*, notificationTemplates*/ };
+export { components, schema, locales, composables };

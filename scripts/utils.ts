ddce9f8f8a5ed8d211a668f5decdb6688a630cdb
symbot <maxim.zinchuk@gmail.com>
2023-11-09 14:03:38 +0300

    fix(scripts/utils): added @vc-shell/ts-config in boilerplate version bumping

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -7,7 +7,9 @@ export async function updateBoilerplatePkgVersions() {
   const boilerplatePkgPath = "cli/create-vc-app/src/templates/base/package.json";
   const boilerplatePkg = fs.readJsonSync(boilerplatePkgPath);
 
-  ["@vc-shell/api-client-generator"].forEach((dep) => (boilerplatePkg.devDependencies[dep] = "^" + version));
+  ["@vc-shell/api-client-generator", "@vc-shell/ts-config"].forEach(
+    (dep) => (boilerplatePkg.devDependencies[dep] = "^" + version)
+  );
   ["@vc-shell/config-generator", "@vc-shell/framework"].forEach(
     (dep) => (boilerplatePkg.dependencies[dep] = "^" + version)
   );

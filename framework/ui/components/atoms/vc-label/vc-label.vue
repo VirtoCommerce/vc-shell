ab6ad070fdf9aef2be4be10d28d3c964a50c807f
symbot <maxim.zinchuk@gmail.com>
2024-01-03 16:00:58 +0300

    fix(shell): refactor vc-label layout

--- framework/ui/components/atoms/vc-label/vc-label.vue
+++ framework/ui/components/atoms/vc-label/vc-label.vue
@@ -1,35 +1,40 @@
 <template>
-  <div class="tw-flex tw-flex-nowrap tw-font-bold tw-relative">
-    <span class="tw-truncate"><slot></slot></span>
-    <span
-      v-if="required"
-      class="tw-text-[color:var(--label-required-color)] tw-ml-1"
-      >*</span
-    >
-    <span
+  <div class="tw-flex tw-flex-row tw-justify-between tw-items-center">
+    <div class="tw-flex-nowrap tw-font-bold tw-relative tw-truncate">
+      <span class="tw-truncate">
+        <slot></slot>
+      </span>
+      <span
+        v-if="required"
+        class="tw-text-[color:var(--label-required-color)] tw-ml-1"
+        >*</span
+      >
+
+      <span
+        v-if="$slots['tooltip']"
+        class="tw-grow tw-basis-0 tw-ml-1"
+      >
+        <VcIcon
+          class="tw-text-[color:var(--label-tooltip-color)]"
+          :icon="tooltipIcon"
+          size="s"
+          @mouseenter="tooltipVisible = true"
+          @mouseleave="tooltipVisible = false"
+        ></VcIcon>
+        <span
+          v-if="tooltipVisible"
+          class="tw-absolute tw-z-10 tw-bg-white tw-border tw-border-solid tw-border-[color:#eef0f2] tw-shadow-[1px_1px_8px_rgba(126,142,157,0.25)] tw-rounded-[3px] tw-text-[color:#8e9daa] tw-font-normal tw-py-1 tw-px-2 tw-ml-4"
+        >
+          <slot name="tooltip"></slot>
+        </span>
+      </span>
+    </div>
+    <div
       v-if="multilanguage"
-      class="tw-text-[color:var(--app-menu-item-icon-color)] tw-absolute tw-right-0"
+      class="tw-text-[color:var(--app-menu-item-icon-color)] tw-shrink-0"
     >
       {{ currentLanguage }}
-    </span>
-    <span
-      v-if="$slots['tooltip']"
-      class="tw-grow tw-basis-0 tw-ml-1"
-    >
-      <VcIcon
-        class="tw-text-[color:var(--label-tooltip-color)]"
-        :icon="tooltipIcon"
-        size="s"
-        @mouseenter="tooltipVisible = true"
-        @mouseleave="tooltipVisible = false"
-      ></VcIcon>
-      <span
-        v-if="tooltipVisible"
-        class="tw-absolute tw-z-10 tw-bg-white tw-border tw-border-solid tw-border-[color:#eef0f2] tw-shadow-[1px_1px_8px_rgba(126,142,157,0.25)] tw-rounded-[3px] tw-text-[color:#8e9daa] tw-font-normal tw-py-1 tw-px-2 tw-ml-4"
-      >
-        <slot name="tooltip"></slot>
-      </span>
-    </span>
+    </div>
   </div>
 </template>
 

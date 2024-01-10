8a59a636643f94ffac282ac37514a4f253007c7a
symbot <maxim.zinchuk@gmail.com>
2024-01-10 19:11:36 +0300

    feat(shell): base navigator.language i18n locale

--- framework/core/plugins/i18n/index.ts
+++ framework/core/plugins/i18n/index.ts
@@ -3,7 +3,7 @@ import { createI18n } from "vue-i18n";
 const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: "en",
+  locale: navigator.language.split("-")[0],
   fallbackLocale: "en",
   formatFallbackMessages: true,
 });

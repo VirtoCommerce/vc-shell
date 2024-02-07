1332861a333f166a0603071c54e90237ea5c7f77
Maksim Zinchuk <63730586+maksimzinchuk@users.noreply.github.com>
2024-02-07 08:16:17 +0300

    Update index.ts

--- src/modules/rating/components/index.ts
+++ src/modules/rating/components/index.ts
@@ -2,5 +2,6 @@ import ReviewStatus from "./ReviewStatus.vue";
 import RatingDashboardCard from "./RatingDashboardCard.vue";
 import RatingGrid from "./RatingGrid.vue";
 import RatingMobileGridView from "./RatingMobileGridView.vue";
+import Rating from "./rating.vue";
 
-export { ReviewStatus, RatingDashboardCard, RatingGrid, RatingMobileGridView };
+export { ReviewStatus, RatingDashboardCard, RatingGrid, RatingMobileGridView, Rating };

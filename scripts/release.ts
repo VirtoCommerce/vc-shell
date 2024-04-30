28d446212b142260224a3947b0913db79908913b
symbot <maxim.zinchuk@gmail.com>
2024-04-30 17:31:35 +0300

    fix: release script cross-spawn for Windows support

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,18 +1,18 @@
 import { release } from "@vc-shell/release-config";
-import { spawnSync } from "child_process";
+import { sync } from "cross-spawn";
 
 release({
   packages: [".", "src/api_client", "src/modules"],
   toTag: (version) => `v${version}`,
   bumpVersion: async (pkgName, pkgVersion) => {
     const bumpArgs = ["workspace", pkgName, "version", pkgVersion, "--deferred"];
-    await spawnSync("yarn", bumpArgs);
+    await sync("yarn", bumpArgs);
 
     const versionApplyArgs = ["version", "apply", "--all"];
-    await spawnSync("yarn", versionApplyArgs);
+    await sync("yarn", versionApplyArgs);
   },
   generateChangelog: async (pkgName, pkgVersion, workspaceName) => {
     const changelogArgs = ["conventional-changelog", "-p", "angular", "-i", "CHANGELOG.md", "-s", "--commit-path", "."];
-    await spawnSync("npx", changelogArgs, { cwd: workspaceName ? `${workspaceName}` : "." });
+    await sync("npx", changelogArgs, { cwd: workspaceName ? `${workspaceName}` : "." });
   },
 });

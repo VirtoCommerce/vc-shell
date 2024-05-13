e4846a05fdd0448d9eb8cf634f4e917ac62d7576
symbot <maxim.zinchuk@gmail.com>
2024-05-13 17:19:34 +0300

    feat: automatic registration of modules dashboard cards

--- src/modules/orders/components/OrdersDashboardCard.vue
+++ src/modules/orders/components/OrdersDashboardCard.vue
@@ -1,38 +1,40 @@
 <template>
-  <VcCard
-    :header="$t('ORDERS.WIDGET.TITLE')"
-    icon="fas fa-sync-alt"
-    :fill="true"
-  >
-    <template
-      v-if="$isDesktop.value"
-      #actions
+  <VcCol class="tw-p-2 !tw-basis-2/4">
+    <VcCard
+      :header="$t('ORDERS.WIDGET.TITLE')"
+      icon="fas fa-sync-alt"
+      :fill="true"
     >
-      <vc-button
-        small
-        outline
-        @click="() => onItemClick()"
-        >{{ $t("ORDERS.WIDGET.ALL") }}</vc-button
+      <template
+        v-if="$isDesktop.value"
+        #actions
       >
-    </template>
-    <Orders
-      :is-widget-view="true"
-      @item-click="onItemClick"
-    >
-      <template #widget-mobile="{ totalCount, loading }">
-        <div
-          v-loading="loading"
-          class="tw-flex tw-flex-auto tw-flex-col"
+        <vc-button
+          small
+          outline
           @click="() => onItemClick()"
+          >{{ $t("ORDERS.WIDGET.ALL") }}</vc-button
         >
-          <div class="tw-h-px tw-bg-[#e3e7ec] tw-w-full"></div>
-          <div class="tw-text-center tw-m-4 tw-text-[26px] tw-font-medium tw-text-[#319ed4]">
-            {{ totalCount == 0 ? $t("ORDERS.WIDGET.EMPTY") : totalCount }}
-          </div>
-        </div>
       </template>
-    </Orders>
-  </VcCard>
+      <Orders
+        :is-widget-view="true"
+        @item-click="onItemClick"
+      >
+        <template #widget-mobile="{ totalCount, loading }">
+          <div
+            v-loading="loading"
+            class="tw-flex tw-flex-auto tw-flex-col"
+            @click="() => onItemClick()"
+          >
+            <div class="tw-h-px tw-bg-[#e3e7ec] tw-w-full"></div>
+            <div class="tw-text-center tw-m-4 tw-text-[26px] tw-font-medium tw-text-[#319ed4]">
+              {{ totalCount == 0 ? $t("ORDERS.WIDGET.EMPTY") : totalCount }}
+            </div>
+          </div>
+        </template>
+      </Orders>
+    </VcCard>
+  </VcCol>
 </template>
 
 <script setup lang="ts">
--- src/modules/products/components/ProductsDashboardCard.vue
+++ src/modules/products/components/ProductsDashboardCard.vue
@@ -1,39 +1,41 @@
 <template>
-  <VcCard
-    :header="$t('PRODUCTS.WIDGET.TITLE')"
-    icon="fas fa-box-open"
-    :fill="true"
-  >
-    <template
-      v-if="$isDesktop.value"
-      #actions
+  <VcCol class="tw-p-2 !tw-basis-2/4">
+    <VcCard
+      :header="$t('PRODUCTS.WIDGET.TITLE')"
+      icon="fas fa-box-open"
+      :fill="true"
     >
-      <vc-button
-        small
-        outline
-        @click="() => onItemClick()"
-        >{{ $t("PRODUCTS.WIDGET.ALL") }}</vc-button
+      <template
+        v-if="$isDesktop.value"
+        #actions
       >
-    </template>
-    <Products
-      :is-widget-view="true"
-      @item-click="onItemClick"
-      @add="addItem"
-    >
-      <template #widget-mobile="{ totalCount, loading }">
-        <div
-          v-loading="loading"
-          class="tw-flex tw-flex-auto tw-flex-col"
+        <vc-button
+          small
+          outline
           @click="() => onItemClick()"
+          >{{ $t("PRODUCTS.WIDGET.ALL") }}</vc-button
         >
-          <div class="tw-h-px tw-bg-[#e3e7ec] tw-w-full"></div>
-          <div class="tw-text-center tw-m-4 tw-text-[26px] tw-font-medium tw-text-[#319ed4]">
-            {{ totalCount == 0 ? $t("PRODUCTS.WIDGET.EMPTY") : totalCount }}
-          </div>
-        </div>
       </template>
-    </Products>
-  </VcCard>
+      <Products
+        :is-widget-view="true"
+        @item-click="onItemClick"
+        @add="addItem"
+      >
+        <template #widget-mobile="{ totalCount, loading }">
+          <div
+            v-loading="loading"
+            class="tw-flex tw-flex-auto tw-flex-col"
+            @click="() => onItemClick()"
+          >
+            <div class="tw-h-px tw-bg-[#e3e7ec] tw-w-full"></div>
+            <div class="tw-text-center tw-m-4 tw-text-[26px] tw-font-medium tw-text-[#319ed4]">
+              {{ totalCount == 0 ? $t("PRODUCTS.WIDGET.EMPTY") : totalCount }}
+            </div>
+          </div>
+        </template>
+      </Products>
+    </VcCard>
+  </VcCol>
 </template>
 
 <script setup lang="ts">

e136ddf6dadd2ebe5c0b4c1551ec1da1e0140fb1
symbot <maxim.zinchuk@gmail.com>
2024-05-24 11:21:55 +0300

    feat(products): update not found/empty template icon color and size

--- src/modules/products/components/ProductsEmptyGridTemplate.vue
+++ src/modules/products/components/ProductsEmptyGridTemplate.vue
@@ -2,7 +2,8 @@
   <div class="tw-w-full tw-h-full tw-box-border tw-flex tw-flex-col tw-items-center tw-justify-center">
     <VcIcon
       icon="fas fa-box-open"
-      class="!tw-text-[95px] !tw-text-[#329ED4]"
+      class="tw-text-[#A9BFD2]"
+      size="xxxl"
     />
     <div class="tw-m-4 tw-text-xl tw-font-medium">
       {{ $t("PRODUCTS.PAGES.LIST.EMPTY.NO_ITEMS") }}

c3c8c63fa48facf640e182405938bb83f5b5087b
symbot <maxim.zinchuk@gmail.com>
2024-06-17 19:08:42 +0300

    feat: improve loading directive with dynamic z-index support

--- framework/core/directives/loading/index.ts
+++ framework/core/directives/loading/index.ts
@@ -2,5 +2,12 @@ import { DirectiveBinding } from "vue";
 import "./styles.css";
 
 export const loading = (el: HTMLElement, binding: DirectiveBinding) => {
-  el.classList[binding.value ? "add" : "remove"]("v-loading");
+  const zIndex = binding.arg ? parseInt(binding.arg, 10) : 9999;
+  if (binding.value) {
+    el.classList.add("v-loading");
+    el.style.setProperty("--v-loading-z-index", `${zIndex}`);
+  } else {
+    el.classList.remove("v-loading");
+    el.style.removeProperty("--v-loading-z-index");
+  }
 };

e7e034e1a770a19c882723bc0a3406aa4eb917cc
Maksim <maxim.zinchuk@gmail.com>
2024-10-01 23:14:00 +0300

    chore: providers fix

--- framework/shared/components/sign-in/useExternalProvider.ts
+++ framework/shared/components/sign-in/useExternalProvider.ts
@@ -29,52 +29,44 @@ export const useExternalProvider = (): IUseExternalProvider => {
 
   async function externalSignIn(authenticationType: string, oidcUrl: string) {
     try {
-      const origin = location.origin;
-      const finalReturnUrl = location.pathname ?? getReturnUrlValue() ?? "/";
+      let url_ = window.location.origin + "externalsignin?";
+      const returnUrl = window.location.pathname ?? "/";
 
-      if (!authenticationType) {
-        throw new Error("The parameter 'authenticationType' cannot be null or undefined.");
+      const signInData = {
+        providerType: authenticationType,
+      };
+
+      externalSignInStorage.value = signInData;
+
+      if (authenticationType === null) throw new Error("The parameter 'authenticationType' cannot be null.");
+      else {
+        if (authenticationType !== undefined)
+          url_ += "authenticationType=" + encodeURIComponent("" + authenticationType) + "&";
+        if (returnUrl !== undefined) url_ += "returnUrl=" + encodeURIComponent("" + returnUrl) + "&";
       }
+      url_ = url_.replace(/[?&]$/, "");
 
-      // const oidcUrlObject = new URL(oidcUrl, origin);
-      // const callbackUrl = new URL("/auth/callback", origin);
-      const url = new URL("/externalsignin", origin);
-      console.log("url", url);
-
-      // Set query parameters
-      // callbackUrl.searchParams.set("returnUrl", finalReturnUrl);
-      url.searchParams.set("authenticationType", authenticationType);
-      // url.searchParams.set("callbackUrl", callbackUrl.href);
-      url.searchParams.set("returnUrl", finalReturnUrl);
-
-      // Store sign-in data
-      externalSignInStorage.value = { providerType: authenticationType };
-
-      console.log("url", url);
-      // Redirect to the constructed URL
-      location.assign(url);
+      window.location.href = url_;
     } catch (e) {
       console.error(e);
+
       throw e;
     }
   }
 
   async function externalSignOut(authenticationType: string): Promise<void> {
     try {
-      const origin = location.origin;
-      const returnUrl = location.pathname ?? "/";
+      let url_ = window.location.origin + "externalsignin/signout?";
 
-      const url = new URL("/externalsignin/signout", origin);
+      if (authenticationType !== undefined)
+        url_ += "authenticationType=" + encodeURIComponent("" + authenticationType) + "&";
+      if (window.location.pathname !== undefined)
+        url_ += "returnUrl=" + encodeURIComponent("" + window.location.pathname) + "&";
 
-      // Set query parameters
-      url.searchParams.set("authenticationType", authenticationType);
-      url.searchParams.set("returnUrl", returnUrl);
+      url_ = url_.replace(/[?&]$/, "");
+      window.location.href = url_;
 
-      // Clear sign-in data
       externalSignInStorage.value = {};
-
-      // Redirect to the sign-out URL
-      location.assign(url);
     } catch (e) {
       console.error(e);
       throw e;

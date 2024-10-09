6a2e4c8f9089301a6da2a73569f0a08976bb3d59
Maksim <maxim.zinchuk@gmail.com>
2024-10-09 14:58:58 +0300

    feat: mp-products as mixin

--- src/modules/mp-products/composables/useProductsList/index.ts
+++ src/modules/mp-products/composables/useProductsList/index.ts
@@ -3,25 +3,55 @@ import { useProductsList, ProductListScope } from "../../../products/composables
 import { ISearchProductsQuery, SellerProduct } from "@vcmp-vendor-portal/api/marketplacevendor";
 import * as _ from "lodash-es";
 import { ICatalogListEntrySearchCriteria, ListEntryBase } from "@vcmp-vendor-portal/api/catalog";
+import { Ref, ref } from "vue";
 
 export interface ProductsListExtendedScope extends ProductListScope {
   openDetailsBlade: (args?: TOpenBladeArgs) => Promise<void>;
 }
 
-export const useProductsListExtended = (
-  args: ListComposableArgs,
-): UseList<
-  SellerProduct[] | ListEntryBase[],
-  (ISearchProductsQuery | ICatalogListEntrySearchCriteria) & { id?: string },
-  ProductsListExtendedScope
-> => {
-  const { items, load, loading, query, pagination, remove, scope } = useProductsList(args);
+// export const useProductsListExtended = (
+//   args: ListComposableArgs,
+// ): UseList<
+//   SellerProduct[] | ListEntryBase[],
+//   (ISearchProductsQuery | ICatalogListEntrySearchCriteria) & { id?: string },
+//   ProductsListExtendedScope
+// > => {
+//   const { items, load, loading, query, pagination, remove, scope } = useProductsList(args);
+//   const { openBlade, resolveBladeByName } = useBladeNavigation();
+
+//   const loadWrap = async (loadQuery?: ISearchProductsQuery) => {
+//     query.value = Object.assign(query.value, loadQuery, { isPublished: true, searchFromAllSellers: true });
+
+//     await load(query.value);
+//   };
+
+//   async function openDetailsBlade(args?: TOpenBladeArgs) {
+//     await openBlade({
+//       blade: resolveBladeByName("MpProduct"),
+//       ...args,
+//     });
+//   }
+
+//   const extendedScope: ProductsListExtendedScope = _.merge(scope, {
+//     openDetailsBlade,
+//   });
+
+//   return { items, load: loadWrap, loading, query, pagination, remove, scope: extendedScope };
+// };
+
+export const useProductsListExtended = (arg: {
+  scope: Record<string, unknown>;
+  load: (query: ISearchProductsQuery) => Promise<void>;
+  query: Ref<ISearchProductsQuery>;
+}) => {
   const { openBlade, resolveBladeByName } = useBladeNavigation();
+
+  const query = ref<ISearchProductsQuery>({});
 
   const loadWrap = async (loadQuery?: ISearchProductsQuery) => {
     query.value = Object.assign(query.value, loadQuery, { isPublished: true, searchFromAllSellers: true });
 
-    await load(query.value);
+    await arg.load(query.value);
   };
 
   async function openDetailsBlade(args?: TOpenBladeArgs) {
@@ -31,9 +61,13 @@ export const useProductsListExtended = (
     });
   }
 
-  const extendedScope: ProductsListExtendedScope = _.merge(scope, {
+  const scope = {
     openDetailsBlade,
-  });
+  };
 
-  return { items, load: loadWrap, loading, query, pagination, remove, scope: extendedScope };
+  return {
+    scope: _.merge(arg.scope, scope),
+    load: loadWrap,
+    query: _.merge(arg.query, query),
+  };
 };
--- src/modules/mp-products/index.ts
+++ src/modules/mp-products/index.ts
@@ -1,20 +1,14 @@
 import { useProductsListExtended } from "./composables/useProductsList";
 import { createDynamicAppModule } from "@vc-shell/framework";
-import { schema, composables, locales } from "./../products";
 import * as mpLocales from "./locales";
 import overrides from "./schemaOverride";
 
 export default createDynamicAppModule({
-  schema,
-  composables: {
-    useProductDetails: composables.useProductDetails,
-    useProductsList: useProductsListExtended,
-  },
   overrides,
-  locales: {
-    ...locales,
-    ...mpLocales,
+  mixin: {
+    MpProducts: [useProductsListExtended],
   },
+  locales: mpLocales,
 });
 
 export type { ProductsListExtendedScope } from "./composables/useProductsList";

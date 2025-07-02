7c03443880413cd0d24ebeefe8aa8f9f28c47516
Maksim <maxim.zinchuk@gmail.com>
2025-07-02 15:04:50 +0300

    refactor(shims-vue): remove unused dynamic module definitions and clean up global interface

--- cli/create-vc-app/src/templates/base/src/shims-vue.d.ts
+++ cli/create-vc-app/src/templates/base/src/shims-vue.d.ts
@@ -1,64 +1,28 @@
 /* eslint-disable */
-import {CoreBladeAdditionalSettings} from "@vc-shell/framework";
-import {Ref, Plugin} from "vue";
-import {DynamicGridSchema, DynamicDetailsSchema} from "@vc-shell/framework";
-import * as vue from "vue";
-import * as vueRouter from "vue-router";
-import * as veeValidate from "vee-validate";
-import * as vueI18n from "vue-i18n";
-import moment from "moment";
-import type {Component} from "vue";
+
+import { CoreBladeAdditionalSettings } from "@vc-shell/framework";
+import type { Ref } from "vue";
+import type { Composer } from "vue-i18n";
 
 declare module "*.vue" {
-  import type {DefineComponent} from "vue";
+  import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
 
 declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    $hasAccess: (permissions: string | string[]) => boolean;
+  interface ComponentCustomProperties extends _ComponentCustomProperties {
+    $mergeLocaleMessage: Composer<{}, {}, {}, string, never, string>["mergeLocaleMessage"];
+    $hasAccess: (permissions: string | string[] | undefined) => boolean;
     $isPhone: Ref<boolean>;
     $isTablet: Ref<boolean>;
     $isMobile: Ref<boolean>;
     $isDesktop: Ref<boolean>;
     $isTouch: boolean;
     $t: (key: string, ...args: any[]) => string;
-    $dynamicModules: {
-      [x: string]: {
-        components?: { [key: string]: Component };
-        composables?: { [key: string]: (...args: any[]) => any };
-        default: { install: (app: any, options?: any) => void };
-        schema: { [key: string]: DynamicGridSchema | DynamicDetailsSchema };
-        locales: { [key: string]: { [key: string]: string } };
-        notificationTemplates?: { [key: string]: Component };
-      };
-    };
   }
 
-  interface ComponentOptionsBase extends CoreBladeAdditionalSettings {
-  }
-}
-
-declare global {
-  interface Window {
-    VcShellDynamicModules: {
-      [x: string]: {
-        components?: { [key: string]: Component };
-        composables?: { [key: string]: (...args: any[]) => any };
-        default: { install: (app: any, options?: any) => void };
-        schema: { [key: string]: DynamicGridSchema | DynamicDetailsSchema };
-        locales: { [key: string]: { [key: string]: string } };
-        notificationTemplates?: { [key: string]: Component };
-      };
-    };
-    Vue: typeof vue;
-    VueRouter: typeof vueRouter;
-    VeeValidate: typeof veeValidate;
-    VueI18n: typeof vueI18n;
-    moment: moment;
-    VcShellFramework: any;
-  }
+  interface ComponentOptionsBase extends CoreBladeAdditionalSettings {}
 }
 
 export {};

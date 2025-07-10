5ba38e8aa81595a08052df23cc7962c69c9f6801
Maksim <maxim.zinchuk@gmail.com>
2025-07-10 13:47:52 +0300

    refactor(composables): remove unused useBladeMultilanguage composable

--- framework/shared/composables/index.ts
+++ framework/shared/composables/index.ts
@@ -1,5 +1,4 @@
 export * from "./useMenuExpanded";
 export * from "./useModificationTracker";
 export * from "./useTableSort";
-export * from "./useBladeMultilanguage";
 export * from "./useExternalWidgets";

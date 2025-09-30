31019a078cb3e3caede67bf8396dc0d985015f6c
Maksim <maxim.zinchuk@gmail.com>
2025-09-30 10:54:42 +0300

    feat(vc-form): implement submit event emission in form component

--- framework/ui/components/molecules/vc-form/vc-form.vue
+++ framework/ui/components/molecules/vc-form/vc-form.vue
@@ -1,7 +1,15 @@
-<script lang="ts" setup></script>
-
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <slot>vc-form</slot>
   </form>
 </template>
+
+<script lang="ts" setup>
+const emit = defineEmits<{
+  (e: 'submit'): void;
+}>()
+
+const submit = () => {
+  emit('submit');
+}
+</script>

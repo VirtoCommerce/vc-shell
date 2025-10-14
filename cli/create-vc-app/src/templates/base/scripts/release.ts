43adccd4537e6d146542ba7ac106a19c36b114f3
Maksim <maxim.zinchuk@gmail.com>
2025-10-14 15:07:31 +0300

    chore: update package dependencies to use workspace references

--- cli/create-vc-app/src/templates/base/scripts/release.ts
+++ cli/create-vc-app/src/templates/base/scripts/release.ts
@@ -1,18 +1,5 @@
 import { release } from "@vc-shell/release-config";
-import { sync } from "cross-spawn";
 
 release({
-  packages: ["."],
-  toTag: (version) => `v${version}`,
-  bumpVersion: async (pkgName, pkgVersion) => {
-    const bumpArgs = ["workspace", pkgName, "version", pkgVersion, "--deferred"];
-    await sync("yarn", bumpArgs);
-
-    const versionApplyArgs = ["version", "apply", "--all"];
-    await sync("yarn", versionApplyArgs);
-  },
-  generateChangelog: async (pkgName, pkgVersion, workspaceName) => {
-    const changelogArgs = ["conventional-changelog", "-p", "angular", "-i", "CHANGELOG.md", "-s", "--commit-path", "."];
-    await sync("npx", changelogArgs, { cwd: workspaceName ? `${workspaceName}` : "." });
-  },
+  packages: ["."]
 });

4a62db7e7af0c1ac41b675a4aedb1d071de0aa87
Maksim <maxim.zinchuk@gmail.com>
2026-03-12 12:51:28 +0300

    refactor(vc-select): remove old composables replaced by useSelectDataSource
    Deleted: useSelectDefaultValue, useSelectOptions, useSelectSearch.
    Updated composables/index.ts exports.

--- framework/ui/components/molecules/vc-select/composables/index.ts
+++ framework/ui/components/molecules/vc-select/composables/index.ts
@@ -1,7 +1,5 @@
 export { useSelectValueMapping, type OptionProp } from "@ui/components/molecules/vc-select/composables/useSelectValueMapping";
 export { useSelectVisibility } from "@ui/components/molecules/vc-select/composables/useSelectVisibility";
 export { useSelectDropdown } from "@ui/components/molecules/vc-select/composables/useSelectDropdown";
-export { useSelectDefaultValue } from "@ui/components/molecules/vc-select/composables/useSelectDefaultValue";
-export { useSelectOptions } from "@ui/components/molecules/vc-select/composables/useSelectOptions";
-export { useSelectSearch } from "@ui/components/molecules/vc-select/composables/useSelectSearch";
+export { useSelectDataSource } from "@ui/components/molecules/vc-select/composables/useSelectDataSource";
 export { useSelectSelection } from "@ui/components/molecules/vc-select/composables/useSelectSelection";
